1a0d8978ffa060867be8a5fee42557df26d7e955
ishan2op <ishankhurana176@gmail.com>
2024-09-07 00:07:45 +0530

    vercel analytics

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,13 @@ import {RecoilRoot} from "recoil";
 import UserChannelReport from './UserChannelReport';
 import Nav from './Nav';
 import AuthMiddleware from './AuthMiddleware';
+import { Analytics } from "@vercel/analytics/react"
 function App() {
 return(
   <>
 
   <Nav />
-
+  <Analytics />
 
   </>
 )
